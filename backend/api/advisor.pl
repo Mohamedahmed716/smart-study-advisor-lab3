11a44da43172a5282d83f45f4546d76942714798
Mohamedahmed716 <es.Mohamednomier2023@alexu.edu.eg>
2026-05-08 22:33:56 +0300

    Finalized the Backend

---
+++ backend/api/advisor.pl
@@ -0,0 +1,6 @@
+% advisor.pl - Placeholder logic
+
+% Format: recommend(StudentName, CourseName).
+recommend(alice, 'Artificial Intelligence').
+recommend(alice, 'Programming Languages').
+recommend(bob, 'Operating Systems').
